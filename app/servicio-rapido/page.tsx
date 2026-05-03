fe62432fa8fef5bf1433e33225fd63967ec2a9fa
xniffer1-ops <xniffer1@gmail.com>
2026-05-03 11:24:33 -0500

    Correccion toneladas

--- app/servicio-rapido/page.tsx
+++ app/servicio-rapido/page.tsx
@@ -1326,7 +1326,7 @@ export default function ServicioRapidoPage() {
             <div style={styles.summaryRow}>
               <span style={styles.summaryLabel}>Cantidad calculada</span>
               <span style={styles.summaryValue}>
-                {formatearToneladas(cantidadNumero)} toneladas
+                {formatearToneladas(cantidadNumero)}
               </span>
             </div>
 
